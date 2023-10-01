14fcfe3ec7b3df258dab24f370e1d69a7f2c49fe
isabellexvr <isabellexavier59@gmail.com>
2023-10-01 15:21:17 -0300

    fix: error handling

--- src/routes/chavePub.cjs
+++ src/routes/chavePub.cjs
@@ -1,7 +1,7 @@
 const express = require("express");
 const addon = require("../c functions/chavePub/build/Release/addon.node");
-const fs = require('fs'), path = require('path').resolve("./");
-
+const fs = require("fs"),
+  path = require("path").resolve("./");
 
 const chavePubRouter = express.Router();
 
@@ -15,26 +15,28 @@ chavePubRouter.post("/public-key", async (req, res) => {
 
   const result = addon.chavePubCpp(Number(p), Number(q), Number(e));
 
-  if(result == 1){
+  if (result == 1) {
     const filePath = path + "/chave-publica.txt";
-    fs.readFile(filePath, "utf8",(err, data) => {
+    fs.readFile(filePath, "utf8", (err, data) => {
       if (err) {
         console.error(err);
-        res.send(err).status(403)
+        res.send(err).status(403);
         return;
       }
-      res.send(data).status(200)
+      res.send(data).status(200);
     });
-
-    //res.send(`(${p * q},${e})`).status(200);
-  }else if(result == 2){
-    res.send("O expoente não é relativamente primo a n.").status(400);
-  }else if(result == 3){
-    res.send("q não é primo.").status(400);
-  }else if(result == 4){
-    res.send("p não é primo.").status(400);
-  }else if(result == 5){
-    res.send("p q q devem ser maiores que 10, para evitar que o bloco ASCII não ser maior que n.").status(400);
+  } else if (result == 2) {
+    res.status(400).send("O expoente não é relativamente primo a n.");
+  } else if (result == 3) {
+    res.status(400).send("q não é primo.");
+  } else if (result == 4) {
+    res.status(400).send("p não é primo.");
+  } else if (result == 5) {
+    res
+      .status(400)
+      .send(
+        "p q q devem ser maiores que 10, para evitar que o bloco ASCII não ser maior que n."
+      );
   }
   return;
 });
